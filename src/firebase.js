6a45f307351634bbc1985e8476bccbc14a118954
Christin98 <christinkoshy1998@gmail.com>
2021-03-02 22:07:50 +0530

    Initialize project using Create React App

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC9WNyB4LEv-rLwBib-ELWPmsYyf4BXfcM",
+    authDomain: "clone-e987f.firebaseapp.com",
+    projectId: "clone-e987f",
+    storageBucket: "clone-e987f.appspot.com",
+    messagingSenderId: "259447301987",
+    appId: "1:259447301987:web:5a3e62b7be84bc208b9556",
+    measurementId: "G-SSV84EX8QC"
+  };
