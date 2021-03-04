6e0dbe897aa3aca2b763b0d78733bd50a37087bc
Christin98 <christinkoshy1998@gmail.com>
2021-03-04 22:17:10 +0530

    Added Header , Sidebar, Recommended Videos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from "react"
 import Header from "./Header"
+import Sidebar from './Sidebar'
+import RecommendedVideos from './RecommendedVideos'
 import './App.css';
 
 function App() {
   return (
     //BEM Naming Convention
     <div className="app">
-     <h1> YAY , Lets Build YouTube Clone </h1>
+     {/* <h1> YAY , Lets Build YouTube Clone </h1> */}
 
      {/* Header */}
 
      <Header/>
 
-     {/* Sidebar */}
-     {/* Recommended Video */}
+     <div className="app__page">
+       {/* Sidebar */}
+       <Sidebar />
+       {/* Recommended Video */}
+       <RecommendedVideos />
+     </div>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,38 @@
 import React from 'react'
+import './Header.css'
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import VideoCallIcon from '@material-ui/icons/VideoCall';
 import AppsIcon from '@material-ui/icons/Apps';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import Avatar from '@material-ui/core/Avatar';
+
 
 function Header() {
     return (
         <div className="header">
-            <h1>I am a header</h1>
-            <MenuIcon />
-            <img 
-            className="header__logo"
-            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" 
-            alt=""/>
-            <input type="text"/>
-            <SearchIcon />
-            <VideoCallIcon />
-            <AppsIcon />
-            <NotificationsIcon />
+            {/* <h1>I am a header</h1> */}
+            <div className="header__left">
+                <MenuIcon />
+                <img 
+                    className="header__logo"
+                    src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg" 
+                    alt=""
+                />
+            </div>
+            <div className="header__input">
+                <input placeholder="Search" type="text"/>
+                <SearchIcon className="header__inputButton"/>
+            </div>
+            <div className="header__icons">
+                <VideoCallIcon className="header__icon"/>
+                <AppsIcon className="header__icon"/>
+                <NotificationsIcon className="header__icon"/>
+                <Avatar
+                    alt="Christin Koshy"
+                    src="https://avatars.githubusercontent.com/u/43451474?s=60&v=4"
+                />
+            </div>
         </div>
     )
 }
